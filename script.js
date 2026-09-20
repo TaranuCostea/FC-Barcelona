1c67c0170a05121de1e7d2a907032a9ef5fc02e8
TaranuCostea <constantintaranu8@gmail.com>
2026-09-20 18:16:19 +0300

    Noua schimbare

---
+++ script.js
@@ -0,0 +1,6 @@
+const navToggleBtn = document.querySelector('#navToggleBtn');
+const nav = document.querySelector('nav.navigation');
+
+navToggleBtn.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
